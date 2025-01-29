a024606e4da15543ece25cd0160393caf40d6968
jenan1234 <jenanaldweab@gmail.com>
2025-01-29 12:21:44 +0300

    commit

---
+++ src/components/task2.jsx
@@ -0,0 +1,9 @@
+import React from"react";
+function task2(props) {
+  return (
+    <div>
+      <button> {props.label}</button>
+    </div>
+  );
+}
+export default task2;
---
+++ src/components/task3.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Add(props) {
+  return (
+    <div style={{ background: "orange", width: "200px" }}>
+      <h3> product details</h3>
+      <ul>
+        <li>{props.PN}</li>
+        <li>{props.price}</li>
+        <li>{props.description}</li>
+      </ul>
+    </div>
+  );
+}
